7b59fe6e34dda7e62537cb951b4b18370b79b874
Paul Tarvydas <guitarvydas@gmail.com>
2021-07-20 00:39:35 -0400

    decodeMx

---
+++ support.js
@@ -0,0 +1,7 @@
+function decodeMxDiagram (encoded) {
+    var data = window.atob (encoded);
+    var inf = pako.inflateRaw (
+	Uint8Array.from (data, c=>c.charCodeAt (0)), {to: 'string'})
+    var str = decodeURIComponent (inf);
+    return str;
+}
