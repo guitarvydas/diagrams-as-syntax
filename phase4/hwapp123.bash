3265acebbd06782b81bb67c84a88ef3e22d81f62
Paul Tarvydas <guitarvydas@gmail.com>
2021-08-18 10:07:39 -0400

    hwapp123__hwsub23.gsh doesn't need pipes

--- phase4/hwapp123.bash
+++ phase4/hwapp123.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -v -x
 # hwapp123
 # inputs: 3 ("in"): <line of characters>
 # outputs: 4 ("out"): <line of characters>
 BIN=.
 port_in=/dev/fd/3
 port_out=/dev/fd/4
-${BIN}/hwapp123__hwsub23.bash 3<${port_in} 4>${port_out} &
+# ${BIN}/hwapp123__hwsub23.bash 3<${port_in} 4>${port_out} &
+grash hwapp123__hwsub23.gsh 3<${port_in} 4>${port_out} &
+
